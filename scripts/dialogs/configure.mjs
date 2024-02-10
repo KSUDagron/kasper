71e4db120aa38f7308df900ab79eb4f4751df415
Jonathan Bartel <jm.bartel@gmail.com>
2024-02-10 00:46:56 -0500

    Update configure.mjs

--- scripts/dialogs/configure.mjs
+++ scripts/dialogs/configure.mjs
@@ -16,7 +16,7 @@ export class ConfigureApp extends VueFormApplication {
 			title: `${MODULE.TITLE} | ${MODULE.localize('configure.title')}`,
 			id: `${MODULE.ID}-configure`,
 			classes: [],
-			width: window.innerWidth > 1000 ? 1000 : window.innerWidth - 100,
+			width: window.innerWidth > 1000 ? 1000 : window.innerWidth + 100,
 			//width: window.innerWidth + 100,
 			height: "auto",
 			resizable: true,
